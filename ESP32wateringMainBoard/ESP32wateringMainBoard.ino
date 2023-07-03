de37bc9f76abbf5cbc6ee1391cc3d3bf725caf3e
Eva Baranyi <ebaranyi@lenovo.com>
2023-07-03 09:03:33 +0200

    first working version of watering

--- ESP32wateringMainBoard/ESP32wateringMainBoard.ino
+++ ESP32wateringMainBoard/ESP32wateringMainBoard.ino
@@ -1,10 +1,56 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <iostream>
+#include <string>
 
-#define TIME_TO_WATER 30000
-#define DELAY 20000
+#define TIME_TO_WATER 300000
+#define DELAY 60000
+#define COUNT 10
+
+WiFiClientSecure client;
+
+const char* ssid = "DIGI_0b2d70";
+const char* password = "aa21f4d2";
+const char* server = "turazzunk.hu";
+const char* url = "/watering/uploadSensorData.php";
+const char* test_root_ca= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFuzCCA6OgAwIBAgIIVwoRl0LE48wwDQYJKoZIhvcNAQELBQAwazELMAkGA1UE\n" \
+"BhMCSVQxDjAMBgNVBAcMBU1pbGFuMSMwIQYDVQQKDBpBY3RhbGlzIFMucC5BLi8w\n" \
+"MzM1ODUyMDk2NzEnMCUGA1UEAwweQWN0YWxpcyBBdXRoZW50aWNhdGlvbiBSb290\n" \
+"IENBMB4XDTExMDkyMjExMjIwMloXDTMwMDkyMjExMjIwMlowazELMAkGA1UEBhMC\n" \
+"SVQxDjAMBgNVBAcMBU1pbGFuMSMwIQYDVQQKDBpBY3RhbGlzIFMucC5BLi8wMzM1\n" \
+"ODUyMDk2NzEnMCUGA1UEAwweQWN0YWxpcyBBdXRoZW50aWNhdGlvbiBSb290IENB\n" \
+"MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAp8bEpSmkLO/lGMWwUKNv\n" \
+"UTufClrJwkg4CsIcoBh/kbWHuUA/3R1oHwiD1S0eiKD4j1aPbZkCkpAW1V8IbInX\n" \
+"4ay8IMKx4INRimlNAJZaby/ARH6jDuSRzVju3PvHHkVH3Se5CAGfpiEd9UEtL0z9\n" \
+"KK3giq0itFZljoZUj5NDKd45RnijMCO6zfB9E1fAXdKDa0hMxKufgFpbOr3JpyI/\n" \
+"gCczWw63igxdBzcIy2zSekciRDXFzMwujt0q7bd9Zg1fYVEiVRvjRuPjPdA1Yprb\n" \
+"rxTIW6HMiRvhMCb8oJsfgadHHwTrozmSBp+Z07/T6k9QnBn+locePGX2oxgkg4YQ\n" \
+"51Q+qDp2JE+BIcXjDwL4k5RHILv+1A7TaLndxHqEguNTVHnd25zS8gebLra8Pu2F\n" \
+"be8lEfKXGkJh90qX6IuxEAf6ZYGyojnP9zz/GPvG8VqLWeICrHuS0E4UT1lF9gxe\n" \
+"KF+w6D9Fz8+vm2/7hNN3WpVvrJSEnu68wEqPSpP4RCHiMUVhUE4Q2OM1fEwZtN4F\n" \
+"v6MGn8i1zeQf1xcGDXqVdFUNaBr8EBtiZJ1t4JWgw5QHVw0U5r0F+7if5t+L4sbn\n" \
+"fpb2U8WANFAoWPASUHEXMLrmeGO89LKtmyuy/uE5jF66CyCU3nuDuP/jVo23Eek7\n" \
+"jPKxwV2dpAtMK9myGPW1n0sCAwEAAaNjMGEwHQYDVR0OBBYEFFLYiDrIn3hm7Ynz\n" \
+"ezhwlMkCAjbQMA8GA1UdEwEB/wQFMAMBAf8wHwYDVR0jBBgwFoAUUtiIOsifeGbt\n" \
+"ifN7OHCUyQICNtAwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3DQEBCwUAA4ICAQAL\n" \
+"e3KHwGCmSUyIWOYdiPcUZEim2FgKDk8TNd81HdTtBjHIgT5q1d07GjLukD0R0i70\n" \
+"jsNjLiNmsGe+b7bAEzlgqqI0JZN1Ut6nna0Oh4lScWoWPBkdg/iaKWW+9D+a2fDz\n" \
+"WochcYBNy+A4mz+7+uAwTc+G02UQGRjRlwKxK3JCaKygvU5a2hi/a5iB0P2avl4V\n" \
+"SM0RFbnAKVy06Ij3Pjaut2L9HmLecHgQHEhb2rykOLpn7VU+Xlff1ANATIGk0k9j\n" \
+"pwlCCRT8AKnCgHNPLsBA2RF7SOp6AsDT6ygBJlh0wcBzIm2Tlf05fbsq4/aC4yyX\n" \
+"X04fkZT6/iyj2HYauE2yOE+b+h1IYHkm4vP9qdCa6HCPSXrW5b0KDtst842/6+Ok\n" \
+"fcvHlXHo2qN8xcL4dJIEG4aspCJTQLas/kx2z/uUMsA1n3Y/buWQbqCmJqK4LL7R\n" \
+"K4X9p2jIugErsWx0Hbhzlefut8cl8ABMALJ+tguLHPPAUJ4lueAI3jZm/zel0btU\n" \
+"ZCzJJ7VLkn5l/9Mt4blOvH+kQSGQQXemOR/qnuOf0GZvBeyqdn6/axag67XH/JJU\n" \
+"LysRJyU3eExRarDzzFhdFPFqSBX/wge2sY0PjlxQRrM9vwGYT7JZVEc+NHt4bVaT\n" \
+"LnPqZih4zR0Uv6CPLy64Lo7yFIrM6bV8+2ydDKXhlg==\n" \
+"-----END CERTIFICATE-----\n";
 
 uint8_t relayPins[4] = {33, 32, 26, 25};
+int counter = 0;
 
 typedef struct struct_message {
   int sensorNumber;
@@ -16,15 +62,16 @@ struct_message sensorData;
 typedef struct circle_data {
     int number;
     bool relayMode;
+    uint relayMinutes;   
     int sensorValue;
     unsigned long timeStarted;
 } circle_data;
 
 circle_data circles[] = {
-    { .number = 1, .relayMode = false, .sensorValue = 100, .timeStarted = 0 },
-    { .number = 2, .relayMode = false, .sensorValue = 100, .timeStarted = 0 },
-    { .number = 3, .relayMode = false, .sensorValue = 100, .timeStarted = 0 },
-    { .number = 4, .relayMode = false, .sensorValue = 100, .timeStarted = 0 }
+    { .number = 1, .relayMode = false, .relayMinutes = 0, .sensorValue = 100, .timeStarted = 0 },
+    { .number = 2, .relayMode = false, .relayMinutes = 0, .sensorValue = 100, .timeStarted = 0 },
+    { .number = 3, .relayMode = false, .relayMinutes = 0, .sensorValue = 100, .timeStarted = 0 },
+    { .number = 4, .relayMode = false, .relayMinutes = 0, .sensorValue = 100, .timeStarted = 0 }
 };
 
 void checkRelays() {
@@ -37,6 +84,7 @@ void checkRelays() {
     Serial.print(circles[i].relayMode);
 
     if (circles[i].relayMode == true) {
+      circles[i].relayMinutes += 1;
       if (circles[i].timeStarted + (TIME_TO_WATER) < millis()) {
         digitalWrite(relayPins[i], LOW);
         circles[i].relayMode = false;
@@ -51,18 +99,26 @@ void checkRelays() {
   }
 }
 
+void resetRelayMinutes() {
+  unsigned int i = 0;
+
+  for (i = 0; i<4; i++) {
+    circles[i].relayMinutes = 0;
+  }
+}
+
 void onDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&sensorData, incomingData, sizeof(sensorData));
-  Serial.print("Data received: ");
-  Serial.println(len);
   Serial.print("Sensor number: ");
   Serial.println(sensorData.sensorNumber);
   Serial.print("Moisture value: ");
   Serial.println(sensorData.mappedValue);
   Serial.println();
 
-  if (sensorData.mappedValue < 20) {
-    unsigned int index = sensorData.sensorNumber - 1;
+  unsigned int index = sensorData.sensorNumber - 1;
+  circles[index].sensorValue = sensorData.mappedValue;
+
+  if (sensorData.mappedValue < 20) {  
     if (circles[index].relayMode == false) {
       circles[index].relayMode = true;
       digitalWrite(relayPins[index], HIGH);
@@ -71,26 +127,82 @@ void onDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   }  
 }
 
+void sendDataToServer() {
+  WiFiClientSecure client;
+
+  client.setCACert(test_root_ca);
+
+  String postData = "sensor={\"soil1\": " + String(circles[0].sensorValue) 
+    + ", \"soil2\": " + String(circles[1].sensorValue) 
+    + ", \"soil3\": " + String(circles[2].sensorValue)
+    + ", \"soil4\": " + String(circles[3].sensorValue) 
+    + ", \"relay1\": " + String(circles[0].relayMinutes)
+    + ", \"relay2\": " + String(circles[1].relayMinutes)
+    + ", \"relay3\": " + String(circles[2].relayMinutes)
+    + ", \"relay4\": " + String(circles[3].relayMinutes) + "}";
+
+  Serial.println(postData);
+
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(server, 443))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+    // Make a HTTP request:
+    client.println(String("POST https://") + server + url + " HTTP/1.0");
+    client.println(String("Host: ") + server);
+    client.println("Content-type: application/x-www-form-urlencoded");   
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
+
+    client.stop();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP_STA);
 
   pinMode(relayPins[0], OUTPUT);
   pinMode(relayPins[1], OUTPUT);
   pinMode(relayPins[2], OUTPUT);
-  pinMode(relayPins[3], OUTPUT);  
+  pinMode(relayPins[3], OUTPUT);
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP_NOW");
 
     return;
-  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 
-  esp_now_register_recv_cb(onDataRecv);
+  esp_now_register_recv_cb(onDataRecv);  
+
+  WiFi.begin(ssid, password);
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // wait 1 second for re-trying
+    delay(1000);
+  }
+
+  Serial.print("Wi-Fi Channel: ");
+  Serial.println(WiFi.channel());
 }
 
 
 void loop() {
   checkRelays();
+  Serial.println(counter);
+  if (counter == COUNT - 1) {
+    sendDataToServer();
+    resetRelayMinutes();
+  }
+  counter += 1;
+  if (counter == COUNT) {
+    counter = 0;
+  }
   delay(DELAY);
 }
--- ESP32wateringSensorBoard/ESP32wateringSensorBoard.ino
+++ ESP32wateringSensorBoard/ESP32wateringSensorBoard.ino
@@ -1,12 +1,14 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <esp_wifi.h>
 
 #define SECONDFACTOR 1000000  
-#define TIME_TO_SLEEP  10
+#define TIME_TO_SLEEP  600
 
-const int SensorNumber = 2;
+const int SensorNumber = 4;
 const int minValue = 878;
-const int maxValue = 2667;
+const int maxValue = 2290;
+const char* ssid = "DIGI_0b2d70";
 
 const int SensorPin = 32;
 int readValue;
@@ -28,13 +30,33 @@ void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void onSendFail() {
+  Serial.println("Sending data failed!");
   esp_deep_sleep_start();
+}
+
+int32_t obtain_wifi(const char *ssid) {
+  Serial.println("Function obtain_wifi called");
+  if (int32_t n = WiFi.scanNetworks()) {
+      for (uint8_t i=0; i<n; i++) {
+          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+              return WiFi.channel(i);
+          }
+      }
+  }
+  return 0;
 }
 
 void setup() {
   WiFi.mode(WIFI_STA);
 
+  Serial.begin(115200);
+
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * SECONDFACTOR);
+
+  int32_t channel = obtain_wifi(ssid);
+  Serial.print("Channel: ");
+  Serial.println(channel);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 
   readValue = analogRead(SensorPin);
   mappedValue = map(readValue, minValue, maxValue, 100, 0);
@@ -47,8 +69,11 @@ void setup() {
 
   data.sensorNumber = SensorNumber;
   data.mappedValue = mappedValue;
+  Serial.print("readValue = ");
+  Serial.println(readValue);
 
   if (esp_now_init() != ESP_OK) {
+    Serial.println("Esp now init failed!");
     onSendFail();
     
     return;
@@ -61,9 +86,13 @@ void setup() {
   peerInfo.encrypt = false;
 
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Esp add peer failed!");
     onSendFail();    
     
     return ;
+  } else {
+    Serial.println("ESP add peer succeed!");    
+    
   }
 
   esp_err_t result = esp_now_send(responderAddress, (uint8_t *) &data, sizeof(data));
